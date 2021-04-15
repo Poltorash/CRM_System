eec9f4797452dac695eed4faaf98e0e2073c7e17
aleks <nusberg9100@mail.ru>
2021-04-15 19:20:46 +0500

    UnitTest

---
+++ UnitTestClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestClient")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("adee16f7-1061-4385-8cca-54e88549b44e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestClient/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CRM.Context;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestClient
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var db = new CRM_Model()) 
+            {
+                string titleTest = "test";
+                string lastNameTest = "test";
+                string firstNameTest = "test";
+                string patronymicTest = "test";
+                string phoneTest = "test";
+                string addressCompanyTest = "test";
+                string photoTest = "";
+                string expected = "Запись добавлена!";
+                string actual = db.AddClient(titleTest, lastNameTest, firstNameTest, patronymicTest, phoneTest, addressCompanyTest, photoTest);
+                Assert.AreEqual(expected,actual);
+            }
+        }
+    }
+}
